97d33e6d9b21fc6040289c8366daaa43bdbdf572
mksiq <maickelsiqueira@gmail.com>
2021-05-10 18:30:13 -0400

    added vercel url for prod

--- config/index.tsx
+++ config/index.tsx
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'host';
+export const server = dev
+  ? 'http://localhost:3000'
+  : 'https://salon-everywhere.vercel.app';
