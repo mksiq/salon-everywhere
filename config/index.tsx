af28fa72ab2760c69b6e1f642f51d4c82ea49a4d
mksiq <maickelsiqueira@gmail.com>
2021-05-10 18:53:46 -0400

    changed api route to aws amplify

--- config/index.tsx
+++ config/index.tsx
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://salon-everywhere.vercel.app';
+  : 'https://main.d2q1k6v6pdmo53.amplifyapp.com/';
